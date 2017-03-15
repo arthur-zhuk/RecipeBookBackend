e7ba702e8bf4e2623e058d3744500753ddd8a3be
Arthy <arthurzhuk@gmail.com>
2017-03-15 18:51:29 +0000

    small updates to schema etc.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const mongoose = require("mongoose");
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
 
 // App Setup
 app.use(morgan('combined'));
--- router.js
+++ router.js
@@ -1,5 +1,5 @@
-module.exports = (app) => {
-  app.get('/', (req, res, next) => {
-    res.send(['waterbottle', 'phone', 'paper']); 
-  });
-}
+const Authentication = require('./controllers/authentication');
+
+module.exports = function(app) {
+  app.post('/signup', Authentication.signup);
+};
