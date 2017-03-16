862b32b4322c27889baf2fddfc346acb8768bbae
Arthur Zhuk <arthurzhuk@gmail.com>
2017-03-16 10:15:52 -0700

    auth api server

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define our model
 const userSchema = new Schema({
@@ -7,6 +8,35 @@ const userSchema = new Schema({
   password: String
 })
 
+
+// On save Hook, encrypt password
+// Before saving a model, run this function
+userSchema.pre('save', function(next) {
+  // get access to the user model
+  const user = this; 
+
+  // generate a salt, then run callback
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    // hash (encrypt) our password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }
+
+      // overwrite plain text password with encrypted password
+      user.password = hash;
+      next();
+    })
+  })
+})
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return callback(err);
+
+    callback(null, isMatch);
+  });
+}
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema);
 
